d3f820ecc860135efe44d54f503ce97a8e857333
farid <deltabur@gmail.com>
2025-03-22 07:12:47 -0400

    I modify the main.java file

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
+
+
 public class main{
     public static void main(String[] args) {
         System.out.println("Hello From Debian");
+        String[] names = {"Farid", "Ali", "reza"};
+        for(String name : names){
+            System.out.println(name);
+        }
     }
 }
